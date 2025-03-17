bdbb31818cad64f4f4cf85909ae29b06a9590212
bestia.dev <info@bestia.dev>
2025-03-17 17:54:01 +0100

    update docs

--- docs/crates.js
+++ docs/crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["cargo_auto_template_new_cli","cargo_auto_template_new_cli_lib"];
+//{"start":21,"fragment_lengths":[29,34]}
--- docs/src-files.js
+++ docs/src-files.js
@@ -1,5 +1,3 @@
-var srcIndex = new Map(JSON.parse('[\
-["cargo_auto_template_new_cli",["",[],["main.rs"]]],\
-["cargo_auto_template_new_cli_lib",["",[],["hello_mod.rs","lib.rs"]]]\
-]'));
+var srcIndex = new Map(JSON.parse('[["cargo_auto_template_new_cli",["",[],["main.rs"]]],["cargo_auto_template_new_cli_lib",["",[],["hello_mod.rs","lib.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[51,70]}
