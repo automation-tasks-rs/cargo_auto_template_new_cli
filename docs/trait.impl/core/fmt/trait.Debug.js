1ed1f9ee80cf550edea563bc411ec6964d322423
bestia.dev <info@bestia.dev>
2025-10-03 09:35:56 +0000

    update docs

--- docs/trait.impl/core/fmt/trait.Debug.js
+++ docs/trait.impl/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["cargo_auto_template_new_cli_lib",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"cargo_auto_template_new_cli_lib/enum.LibraryError.html\" title=\"enum cargo_auto_template_new_cli_lib::LibraryError\">LibraryError</a>"]]]]);
+    var implementors = Object.fromEntries([["cargo_auto_template_new_cli_lib",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"cargo_auto_template_new_cli_lib/enum.LibraryError.html\" title=\"enum cargo_auto_template_new_cli_lib::LibraryError\">LibraryError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
