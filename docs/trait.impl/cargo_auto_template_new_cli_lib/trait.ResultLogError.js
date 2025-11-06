0ffa2c8fd3a449851d57661ada9f31a070cfc70d
bestia.dev <info@bestia.dev>
2025-11-06 21:26:04 +0000

    update docs

---
+++ docs/trait.impl/cargo_auto_template_new_cli_lib/trait.ResultLogError.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["cargo_auto_template_new_cli_lib",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[38]}
